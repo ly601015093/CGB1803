521a86df90aeb8b28c7704470b24190f9d913281
liyue <601015093@qq.com>
2018-08-16 17:58:03 +0800

    nihao

---
+++ src/main/java/com/tedu/EureKaRunApp.java
@@ -0,0 +1,15 @@
+package com.tedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+
+public class EureKaRunApp {
+    public static void main(String[] args) {
+
+        SpringApplication.run(EureKaRunApp.class,args);
+    }
+}
